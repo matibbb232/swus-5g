db6d4eb65917996a6a226152f9e63830483575d9
Mateusz Bagienski <mateuszb6a@gmail.com>
2024-05-30 15:40:52 +0200

    Fix configuration not being valid

--- main.tf
+++ main.tf
@@ -11,48 +11,47 @@ terraform {
 provider "virtualbox" {}
 
 resource "virtualbox_vm" "master" {
-  name = "k8s-master"
-  os_type = "Ubuntu_64"
-  cpus = 2
-  memory = "2048 mib"
+  name     = "k8s-master"
+  image    = "https://cloud-images.ubuntu.com/bionic/current/bionic-server-cloudimg-amd64.vmdk"
+  cpus     = 2
+  memory   = 2048
   network_adapter {
     type           = "hostonly"
     host_interface = "vboxnet0"
-  }
-  disk {
-    size = "20000 mib"
   }
 }
 
 resource "virtualbox_vm" "worker" {
-  count = 2
-  name = "k8s-worker-${count.index + 1}"
-  os_type = "Ubuntu_64"
-  cpus = 2
-  memory = "2048 mib"
+  count    = 2
+  name     = "k8s-worker-${count.index + 1}"
+  image    = "https://cloud-images.ubuntu.com/bionic/current/bionic-server-cloudimg-amd64.vmdk"
+  cpus     = 2
+  memory   = 2048
   network_adapter {
     type           = "hostonly"
     host_interface = "vboxnet0"
-  }
-  disk {
-    size = "20000 mib"
   }
 }
 
 resource "null_resource" "provision_master" {
   depends_on = [virtualbox_vm.master]
 
-  provisioner "local-exec" {
-    command = <<-EOT
-      ssh -o StrictHostKeyChecking=no ubuntu@${virtualbox_vm.master.network_adapter.0.ipv4_address} <<'EOF'
-        sudo kubeadm init --pod-network-cidr=10.244.0.0/16
-        mkdir -p $HOME/.kube
-        sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-        sudo chown $(id -u):$(id -g) $HOME/.kube/config
-        kubectl apply -f https://raw.githubusercontent.com/intel/multus-cni/master/deployments/multus-daemonset.yml
-        curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
-      EOF
-    EOT
+  provisioner "remote-exec" {
+    inline = [
+      "sudo kubeadm init --pod-network-cidr=10.244.0.0/16",
+      "mkdir -p $HOME/.kube",
+      "sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config",
+      "sudo chown $(id -u):$(id -g) $HOME/.kube/config",
+      "kubectl apply -f https://raw.githubusercontent.com/intel/multus-cni/master/deployments/multus-daemonset.yml",
+      "curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash"
+    ]
+
+    connection {
+      type     = "ssh"
+      user     = "ubuntu"
+      private_key = file("~/.ssh/id_ed25519")
+      host     = virtualbox_vm.master.network_adapter[0].ipv4_address
+    }
   }
 }
 
@@ -60,13 +59,17 @@ resource "null_resource" "provision_worker" {
   count = 2
   depends_on = [virtualbox_vm.worker, null_resource.provision_master]
 
-  provisioner "local-exec" {
-    command = <<-EOT
-      ssh -o StrictHostKeyChecking=no ubuntu@${virtualbox_vm.worker[count.index].network_adapter.0.ipv4_address} <<'EOF'
-        sudo kubeadm join ${virtualbox_vm.master.network_adapter.0.ipv4_address}:6443 --token TOKEN --discovery-token-ca-cert-hash sha256:HASH
-        curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
-      EOF
-    EOT
+  provisioner "remote-exec" {
+    inline = [
+      "sudo kubeadm join ${virtualbox_vm.master.network_adapter[0].ipv4_address}:6443 --token <TOKEN> --discovery-token-ca-cert-hash sha256:<HASH>",
+      "curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash"
+    ]
+
+    connection {
+      type     = "ssh"
+      user     = "ubuntu"
+      private_key = file("~/.ssh/id_ed25519")
+      host     = virtualbox_vm.worker[count.index].network_adapter[0].ipv4_address
+    }
   }
 }
-
