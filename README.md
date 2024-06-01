3de8f24ab70e473a9b8b25410becdcb33979fdad
mbagienski <m.bagienski@stud.elka.pw.edu.pl>
2024-06-01 15:00:20 +0200

    mend

--- README.md
+++ README.md
@@ -4,6 +4,21 @@
 - kubectl
 - kubeadm
 
+# Creating and configuring VMs
+https://www.youtube.com/watch?v=EHDDm_iR1Fs
+
+## Create NAT Network
+
+## Create VMs
+Create 1 master node and 2 worker nodes
+Adapter 1: bridged
+Adapter 2: NAT Network, K8s-NAT-Network
+
+Note: If Ubuntu installer is not finding ip address on second adapter, try changing it's MAC address.
+Enable SSH on each one of them.
+
+Paste ubuntu_no_password to /etc/sudoers.d/ubuntu_no_password to disable sudo prompting for password.
+
 # Running terraform
 ```
 terraform init
--- main.tf
+++ main.tf
@@ -1,75 +1,76 @@
 # main.tf
 terraform {
   required_providers {
-    virtualbox = {
-      source = "terra-farm/virtualbox"
-      version = "0.2.2-alpha.1"
-    }
   }
 }
 
-provider "virtualbox" {}
-
-resource "virtualbox_vm" "master" {
-  name     = "k8s-master"
-  image    = "https://cloud-images.ubuntu.com/bionic/current/bionic-server-cloudimg-amd64.vmdk"
-  cpus     = 2
-  memory   = 2048
-  network_adapter {
-    type           = "hostonly"
-    host_interface = "vboxnet0"
-  }
-}
-
-resource "virtualbox_vm" "worker" {
-  count    = 2
-  name     = "k8s-worker-${count.index + 1}"
-  image    = "https://cloud-images.ubuntu.com/bionic/current/bionic-server-cloudimg-amd64.vmdk"
-  cpus     = 2
-  memory   = 2048
-  network_adapter {
-    type           = "hostonly"
-    host_interface = "vboxnet0"
-  }
-}
-
+# k8s-master
 resource "null_resource" "provision_master" {
-  depends_on = [virtualbox_vm.master]
 
   provisioner "remote-exec" {
     inline = [
-      "sudo kubeadm init --pod-network-cidr=10.244.0.0/16",
-      "mkdir -p $HOME/.kube",
-      "sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config",
-      "sudo chown $(id -u):$(id -g) $HOME/.kube/config",
-      "kubectl apply -f https://raw.githubusercontent.com/intel/multus-cni/master/deployments/multus-daemonset.yml",
-      "curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash"
+      "echo ubuntu | sudo su",
+      # "sudo kubeadm init --pod-network-cidr=10.244.0.0/16",
+      # "mkdir -p $HOME/.kube",
+      # "sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config",
+      # "sudo chown $(id -u):$(id -g) $HOME/.kube/config",
+      # "kubectl apply -f https://raw.githubusercontent.com/intel/multus-cni/master/deployments/multus-daemonset.yml",
+      # "curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash"
+      "sudo touch hello_world_2"
     ]
 
     connection {
       type     = "ssh"
       user     = "ubuntu"
-      private_key = file("~/.ssh/id_ed25519")
-      host     = virtualbox_vm.master.network_adapter[0].ipv4_address
+      password = "ubuntu"
+      host = "192.168.1.20"
     }
   }
 }
 
-resource "null_resource" "provision_worker" {
-  count = 2
-  depends_on = [virtualbox_vm.worker, null_resource.provision_master]
 
-  provisioner "remote-exec" {
-    inline = [
-      "sudo kubeadm join ${virtualbox_vm.master.network_adapter[0].ipv4_address}:6443 --token <TOKEN> --discovery-token-ca-cert-hash sha256:<HASH>",
-      "curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash"
-    ]
+# # k8s-worker-1
+# resource "null_resource" "provision_worker_1" {
 
-    connection {
-      type     = "ssh"
-      user     = "ubuntu"
-      private_key = file("~/.ssh/id_ed25519")
-      host     = virtualbox_vm.worker[count.index].network_adapter[0].ipv4_address
-    }
-  }
-}
+#   provisioner "remote-exec" {
+#     inline = [
+#       # "sudo kubeadm init --pod-network-cidr=10.244.0.0/16",
+#       # "mkdir -p $HOME/.kube",
+#       # "sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config",
+#       # "sudo chown $(id -u):$(id -g) $HOME/.kube/config",
+#       # "kubectl apply -f https://raw.githubusercontent.com/intel/multus-cni/master/deployments/multus-daemonset.yml",
+#       # "curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash"
+#       "touch hello_world"
+#     ]
+
+#     connection {
+#       type     = "ssh"
+#       user     = "ubuntu"
+#       password = "ubuntu"
+#       host = "192.168.1.20"
+#     }
+#   }
+# }
+
+# # k8s-worker-2
+# resource "null_resource" "provision_worker_2" {
+
+#   provisioner "remote-exec" {
+#     inline = [
+#       # "sudo kubeadm init --pod-network-cidr=10.244.0.0/16",
+#       # "mkdir -p $HOME/.kube",
+#       # "sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config",
+#       # "sudo chown $(id -u):$(id -g) $HOME/.kube/config",
+#       # "kubectl apply -f https://raw.githubusercontent.com/intel/multus-cni/master/deployments/multus-daemonset.yml",
+#       # "curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash"
+#       "touch hello_world"
+#     ]
+
+#     connection {
+#       type     = "ssh"
+#       user     = "ubuntu"
+#       password = "ubuntu"
+#       host = "192.168.1.20"
+#     }
+#   }
+# }
