3842145cbbb5cf4f0b60b6f28b102d973977cda0
mbagienski <m.bagienski@stud.elka.pw.edu.pl>
2024-06-12 22:07:03 +0200

    Add variables for IP addr and fix script names

--- main.tf
+++ main.tf
@@ -6,15 +6,15 @@ terraform {
 
 variable "k8s-master-ip" {
   type    = string
-  default = "192.168.1.20"
+  default = "192.168.1.16"
 }
 variable "k8s-worker-1-ip" {
   type    = string
-  default = "192.168.1.20"
+  default = "192.168.1.17"
 }
 variable "k8s-worker-2-ip" {
   type    = string
-  default = "192.168.1.20"
+  default = "192.168.1.18"
 }
 
 # k8s-master
@@ -34,15 +34,15 @@ resource "null_resource" "provision_master" {
 
   provisioner "remote-exec" {
     inline = [
-      "chmod +x /home/ubuntu/k8s-setup.sh",
-      "sudo /home/ubuntu/k8s-setup.sh"
+      "chmod +x /home/ubuntu/k8s-master-setup.sh",
+      "sudo /home/ubuntu/k8s-master-setup.sh"
     ]
 
     connection {
       type     = "ssh"
       user     = "ubuntu"
       password = "ubuntu"
-      host     = "192.168.1.20"
+      host     = var.k8s-master-ip
     }
   }
 }
@@ -58,7 +58,7 @@ resource "null_resource" "provision_worker_1" {
       type     = "ssh"
       user     = "ubuntu"
       password = "ubuntu"
-      host     = "192.168.1.20"
+      host     = var.k8s-worker-1-ip
     }
   }
   provisioner "remote-exec" {
@@ -71,7 +71,7 @@ resource "null_resource" "provision_worker_1" {
       type     = "ssh"
       user     = "ubuntu"
       password = "ubuntu"
-      host     = "192.168.1.20"
+      host     = var.k8s-worker-1-ip
     }
   }
 }
@@ -86,7 +86,7 @@ resource "null_resource" "provision_worker_2" {
       type     = "ssh"
       user     = "ubuntu"
       password = "ubuntu"
-      host     = "192.168.1.20"
+      host     = var.k8s-worker-2-ip
     }
   }
   provisioner "remote-exec" {
@@ -99,7 +99,7 @@ resource "null_resource" "provision_worker_2" {
       type     = "ssh"
       user     = "ubuntu"
       password = "ubuntu"
-      host     = "192.168.1.20"
+      host     = var.k8s-worker-2-ip
     }
   }
 }
